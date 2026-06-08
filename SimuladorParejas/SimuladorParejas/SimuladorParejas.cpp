90e7abda277c377ed256588b15df6766e5f7037b
pmorales50911-afk <pmorales50911@ucq.edu.mx>
2026-06-07 22:20:38 -0600

    Primera parte del codigo
    En esta parte del codigo coloque metodos y atributos ademas de colocar preguntas

--- SimuladorParejas/SimuladorParejas/SimuladorParejas.cpp
+++ SimuladorParejas/SimuladorParejas/SimuladorParejas.cpp
@@ -1,19 +1,46 @@
-// SimuladorParejas.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+using namespace std;
+
+// Simulador de parejas compatibles 
+
+class persona {
+private:
+
+    string Nombre;
+    int Edad;
+public:
+    string colorfav;
+    string musicafav;
+    string pasatiempofav;
+
+    persona(string colorfav, string musicafav, string pasatiempofav);
+
+};
+
+void capturarDatos();
+
+class preguntas : public persona {
+
+    void capturarDatos() {
+
+        string colorfav, musicafav, pasatiempofav;
+
+        cout << "¿Cual es tu color favorito?" << endl;
+        cin >> colorfav;
+
+        cout << "¿Cual es tu musica favorita?" << endl;
+        cin >> musicafav;
+
+        cout << "Cual es tu pasatiempo favorito?" << endl;
+        cin >> pasatiempofav;
+    }
+};
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
-}
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
+    return 0;
+};
